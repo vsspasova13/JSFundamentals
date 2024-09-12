416f91aee6dc7427ff8b0f1070bfffc7d834184d
vsspasova13 <vsspasova@abv.bg>
2024-09-12 21:02:24 +0300

    new tasks added

---
+++ DataTypesAndVariables/ex1SumDigits.js
@@ -0,0 +1,13 @@
+function sumDigits(number)
+{
+number=number.toString();
+let sum=0;
+for(let i=0;i<number.length;i++)
+{
+sum=sum+Number(number[i]);
+}
+
+console.log(sum);
+}
+
+sumDigits(12345);

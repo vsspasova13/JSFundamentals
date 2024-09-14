1a85deda394ca1b68229de4e617b68cd95caff01
vsspasova13 <vsspasova@abv.bg>
2024-09-14 16:40:04 +0300

    fixing

--- DataTypesAndVariables/ex9Gladiator.js
+++ DataTypesAndVariables/ex9Gladiator.js
@@ -3,30 +3,32 @@ function GladiatorExpences(lost, helmet,sword,shield,armor)
     let isHelmetBroken=false;
     let isSwordBroken=false; 
     let shieldBrokenTimes=0;
-    let money=0;
+    let money=0.0;
 
     for(let i=1;i<=lost;i++)
     {
         if(i%2===0)
         {
-            
+            isHelmetBroken=true;
             money+=helmet;
         }
         if(i%3===0)
         {
-            
+            isSwordBroken=true;
             money+=sword;
         }
-        if(i%6===0)
+        if(isHelmetBroken===true && isSwordBroken===true)
         {
-            
+            shieldBrokenTimes++;
             money+=shield;
+            if(shieldBrokenTimes%2===0)
+            {
+                money+=armor;
+            }
         }
 
-        if(i%12===0)
-        {
-            money+=armor;
-        }
+        isSwordBroken=false;
+        isHelmetBroken=false;
 
     }
 
@@ -36,5 +38,5 @@ function GladiatorExpences(lost, helmet,sword,shield,armor)
 GladiatorExpences(23,
     12.50,
     21.50,
-    40,
-    200)
+    40.0,
+    200.0)
